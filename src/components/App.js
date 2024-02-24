72d214165d962dbebb2cdaa1e6f1a42a7a984916
Denis Knor <knorred@mail.ru>
2024-02-24 16:55:17 +0300

    feat add header

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,18 @@
 import Header from './Header';
 import logo from './logo.svg';
-import './App.css';
+
 
 //import { Route, Routes, useNavigate } from 'react-router-dom'
 
 function App() {
+  const loggedIn = true;
   return (
     <div className="App">
-      <Header/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      
+      <Header
+        loggedIn={loggedIn}
+      />
+
+
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,30 @@
 import logo from '../images/header__logo.svg';
 import accountIcon from '../images/header__account-icon.svg';
+
 function Header(props) {
     return (
         <header className="header">
-            <img className="header__logo" src={logo} alt="Логотип" />
+            <div className="header__logo-box">
+                <img className="header__logo" src={logo} alt="Логотип" />
+            </div >
             <navi className="header__navigation">
-                <link className="header__navigation-link" href="" />
-                <link className="header__navigation-link" href="" />
+                <button className="header__button header__navigation-button">Фильмы</button>
+                <button className="header__button header__navigation-button">Сохранённые фильмы</button>
             </navi>
             <div className="header__account">
-                <button className="header__account-signup">Регистрация</button>
-                <button className="header__account-signin">Войти</button>
-                <button className="header__account-profile">Аккаунт
+                <button className="header__button header__account-button">Регистрация</button>
+                <button className="header__button header__account-button">Войти</button>
+                <button className="header__button header__account-button">Аккаунт
                     <div className="header__account-logo">
                         <img className="header__account-icon" src={accountIcon} alt="Аккаунт" />
                     </div>
                 </button>
             </div>
+            <button className="header__button header__menu-button">
+                <div className="header__menu-icon"></div>
+                <div className="header__menu-icon"></div>
+                <div className="header__menu-icon"></div>
+            </button>
         </header>
     );
 }
