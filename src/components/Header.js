6537c352c86fab2a8c66a81d4b66fcc6731adbed
Denis Knor <knorred@mail.ru>
2024-02-24 19:08:19 +0300

    fix header buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,14 @@ function Header(props) {
                 <img className="header__logo" src={logo} alt="Логотип" />
             </div >
             <navi className="header__navigation">
-                <button className="header__button header__navigation-button">Фильмы</button>
-                <button className="header__button header__navigation-button">Сохранённые фильмы</button>
+                <button className="header__button">Фильмы</button>
+                <button className="header__button">Сохранённые фильмы</button>
             </navi>
             <div className="header__account">
-                <button className="header__button header__account-button">Регистрация</button>
-                <button className="header__button header__account-button">Войти</button>
-                <button className="header__button header__account-button">Аккаунт
+                <button className="header__button">Регистрация</button>
+                <button className="header__button header__button_green">Войти</button>
+                <button className="header__button header__account-button">
+                    <p className="header__account-button-text">Аккаунт</p>
                     <div className="header__account-logo">
                         <img className="header__account-icon" src={accountIcon} alt="Аккаунт" />
                     </div>
