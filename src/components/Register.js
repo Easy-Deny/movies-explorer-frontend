896529aa71ff37a38e6d3d2fdedf03c1c3b2e514
Denis Knor <knorred@mail.ru>
2024-02-28 20:21:09 +0300

    fix register

--- src/components/Register.js
+++ src/components/Register.js
@@ -7,21 +7,25 @@ function Register(props) {
             <div className="page__logo">
                 <img className="page__logo-icon" src={logo} alt="Логотип" />
             </div>
-            <h2 className="auth__title">Рады видеть!</h2>
+            <h2 className="auth__title">Добро пожаловать!</h2>
             <form className="auth__form" action="#" onSubmit={props.onSubmit}>
+            <p className="auth__text">Имя</p>
+                <input className="auth__input" name="name" type="name" placeholder=""
+                    required minLength="2" maxLength="40" value={props.email} />
+                <span className="auth__text auth__text_red">Error</span>
                 <p className="auth__text">E-mail</p>
-                <input className="auth__input" name="email" type="email" placeholder="Email"
+                <input className="auth__input" name="email" type="email" placeholder=""
                     required minLength="2" maxLength="40" value={props.email} />
                 <span className="auth__text auth__text_red">Error</span>
                 <p className="auth__text">Пароль</p>
                 <input className="auth__input" name="password" type="password"
-                    placeholder="Пароль" required minLength="2" maxLength="200" value={props.password}/>
+                    placeholder="" required minLength="2" maxLength="200" value={props.password}/>
                 <span className="auth__text auth__text_red">Error</span>
-                <button className="auth__button" title="Войти" type="submit">Войти</button>
+                <button className="auth__button" title="Войти" type="submit">Зарегистрироваться</button>
             </form>
             <div className="auth__route">
-                <p className="auth__description">Ещё не зарегистрированы?</p>
-                <Link to="/sign-up" className="auth__link">Регистрация</Link>
+                <p className="auth__description">Уже зарегистрированы?</p>
+                <Link to="/sign-in" className="auth__link">Войти</Link>
             </div>
         </article>
     );
