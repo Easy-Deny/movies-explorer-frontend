7816a504db9c9b57915c59342eeb2e1eacde96ee
Denis Knor <knorred@mail.ru>
2024-02-28 20:15:21 +0300

    fix login

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Main from './Main';
 import Page404 from './Page404';
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import Register from './Register';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Main loggedIn={loggedIn} />} />
           <Route path="/sign-in" element={<Login />} />
+          <Route path="/sign-up" element={<Register />} />
           <Route path='*' element={<Page404 />} />
         </Routes>
       </BrowserRouter>
