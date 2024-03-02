83337232052b79e6b19a152d37d3e839bb6dcb4d
Denis Knor <knorred@mail.ru>
2024-03-02 14:49:59 +0300

    fix 760 and 320 resolution on movies

--- src/temp/cardList.js
+++ src/temp/cardList.js
@@ -23,6 +23,30 @@ const cards =
         name: 'card4',
         link: 'https://sportishka.com/uploads/posts/2023-12/1701459729_sportishka-com-p-kharlei-naklkhed-oboi-48.jpg',
         duration: 'card duration4'
+      },
+      {
+        _id: '65ccf1b82fe7c1e179770998',
+        name: 'card5',
+        link: 'https://sportishka.com/uploads/posts/2023-12/1701459729_sportishka-com-p-kharlei-naklkhed-oboi-48.jpg',
+        duration: 'card duration5'
+      },
+      {
+        _id: '65ccf1b82fe7c1e179770899',
+        name: 'card6',
+        link: 'https://sportishka.com/uploads/posts/2023-12/1701459729_sportishka-com-p-kharlei-naklkhed-oboi-48.jpg',
+        duration: 'card duration6'
+      },
+      {
+        _id: '65ccf1b82fe7c1e179770299',
+        name: 'card7',
+        link: 'https://sportishka.com/uploads/posts/2023-12/1701459729_sportishka-com-p-kharlei-naklkhed-oboi-48.jpg',
+        duration: 'card duration7'
+      },
+      {
+        _id: '65ccf1b82fe7c1e179770939',
+        name: 'card8',
+        link: 'https://sportishka.com/uploads/posts/2023-12/1701459729_sportishka-com-p-kharlei-naklkhed-oboi-48.jpg',
+        duration: 'card duration8'
       }
     ]
 
