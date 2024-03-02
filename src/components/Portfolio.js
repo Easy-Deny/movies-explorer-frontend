16af643ee7eedab66149b4df262ea20d03366282
Denis Knor <knorred@mail.ru>
2024-03-02 16:13:45 +0300

    fix 1280 landing

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,19 +5,25 @@ function Portfolio() {
             <table className="portfolio__table">
                 <tr className="portfolio__table-string">
                     <td className="portfolio__table-column">Статичный сайт</td>
-                    <td className="portfolio__table-column">↗</td>
+                    <td className="portfolio__table-column">
+                        <a href="https://github.com/Easy-Deny" className="portfolio__link">↗</a>
+                    </td>
                 </tr>
                 <tr className="portfolio__table-string">
                     <td className="portfolio__table-column">Адаптивный сайт</td>
-                    <td className="portfolio__table-column">↗</td>
+                    <td className="portfolio__table-column">
+                        <a href="https://github.com/Easy-Deny" className="portfolio__link">↗</a>
+                    </td>
                 </tr>
                 <tr className="portfolio__table-string">
                     <td className="portfolio__table-column">Одностраничное приложение</td>
-                    <td className="portfolio__table-column">↗</td>
+                    <td className="portfolio__table-column">
+                        <a href="https://github.com/Easy-Deny" className="portfolio__link">↗</a>
+                    </td>
                 </tr>
             </table>
 
-           
+
         </article>
     );
 }
--- src/components/Promo.js
+++ src/components/Promo.js
@@ -5,7 +5,7 @@ function Promo() {
         <article className="promo">
             <section className="promo__section">
                 <div className="promo__description">
-                    <h className="promo__heading">Учебный проект студента факультета Веб-разработки.</h>
+                    <h className="promo__heading">Учебный проект студента факультета Веб&#8209;разработки.</h>
                     <p className="promo__text">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
                 </div>
                 <img className="promo__logo" src={promoLogo} alt='Логотип' />
