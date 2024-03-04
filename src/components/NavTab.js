16282a16c0b53ad16d6eae988210691219bc23c0
Denis Knor <knorred@mail.ru>
2024-03-04 23:30:33 +0300

    fix popup

--- src/components/NavTab.js
+++ src/components/NavTab.js
@@ -9,10 +9,10 @@ function NavTab(props) {
                 <button className="popup__close-button" type="button" onClick={props.onClose}></button>
                 <button className={`popup__link ${props.location.pathname==='/' ?  'popup__link_underline': ''}`} onClick={props.goHome}>Главная</button>
                 <button className={`popup__link ${props.location.pathname==='/movies' ?  'popup__link_underline': ''}`} onClick={props.goMovies}>Фильмы</button>
-                <button className={`popup__link ${props.location.pathname==='/saved-movies' ?  'popup__link_underline': ''}`} onClick={props.goSavedMovies}>Сохранённые фильмы</button>
-                <Link to="/profile" className="header__button header__account-button">
+                <button className={`popup__link ${props.location.pathname==='/saved-movies' ?  'popup__link_underline': ''}`} onClick={props.goSavedMovies}>Сохранённые&#32;фильмы</button>
+                <Link to="/profile" className="popup__button">
                     <p className="header__account-button-text">Аккаунт</p>
-                    <div className="header__account-logo">
+                    <div className="header__account-logo popup__account-logo">
                         <img className="header__account-icon" src={accountIcon} alt="Аккаунт" />
                     </div>
                 </Link>
